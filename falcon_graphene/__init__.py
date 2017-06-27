5f7628317c458047fc2755255e36c3099fcbd0ab
Ben Demaree <hello@bendemaree.com>
2017-06-27 14:10:36 -0500

    Improve package version handling

--- falcon_graphene/__init__.py
+++ falcon_graphene/__init__.py
@@ -1,2 +1,4 @@
 from .handlers import GraphQLResource  # noqa
 from .router import GrapheneRouter  # noqa
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+
 from setuptools import setup
 
 
+with open(Path(".") / "falcon_graphene" / "__init__.py", "r") as f:
+    line = [l for l in f if l.startswith("__version__")]
+    if line:
+        exec(line[0])
+    else:
+        raise RuntimeError("No package version found")
+
+
 setup(
-   name="falcon_graphene",
-   version="v0.0.1",
-   packages=["falcon_graphene"]
+    name="falcon_graphene",
+    version=__version__,  # noqa
+    packages=["falcon_graphene"],
+    python_requires="~=3.6"
 )
